b7cea04063bb65c9bb0a6416e98d2f6f72130e55
Aleksei Petrov <aleksei.petrov@five9.com>
2021-09-07 19:22:25 +0300

    Ex 8 : Done

--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -1,35 +1,14 @@
-"""
-Домашнее задание №1
-
-Использование библиотек: ephem
-
-* Установите модуль ephem
-* Добавьте в бота команду /planet, которая будет принимать на вход
-  название планеты на английском, например /planet Mars
-* В функции-обработчике команды из update.message.text получите
-  название планеты (подсказка: используйте .split())
-* При помощи условного оператора if и ephem.constellation научите
-  бота отвечать, в каком созвездии сегодня находится планета.
-
-"""
 import logging
-
+import settings
+import ephem
+import datetime
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
                     filename='bot.log')
 
-
-PROXY = {
-    'proxy_url': 'socks5://t1.learn.python.ru:1080',
-    'urllib3_proxy_kwargs': {
-        'username': 'learn',
-        'password': 'python'
-    }
-}
-
-
+    
 def greet_user(update, context):
     text = 'Вызван /start'
     print(text)
@@ -37,16 +16,35 @@ def greet_user(update, context):
 
 
 def talk_to_me(update, context):
-    user_text = update.message.text
+    user_text = update.message.text 
     print(user_text)
-    update.message.reply_text(text)
+    update.message.reply_text(user_text)
 
+def get_planet_place(update, context): 
+    try:
+        planet = update.message.text.split(" ")
+        if hasattr(ephem, planet[1]):
+            planet_info = getattr(ephem, planet[1])()
+            #print(planet_info)
+            print(planet_info.name)
+            planet_info.compute(datetime.date.today())
+            planet_place = ephem.constellation(planet_info)
+            # print (planet_place)
+            # print(len(planet_place))
+            # print(type(planet_place))
+            update.message.reply_text(planet_place)
+        else:
+             update.message.reply_text("Введенная планета не найдена")            
+    except IndexError:
+         update.message.reply_text("Вы не ввели название планеты")
+ 
 
 def main():
-    mybot = Updater("КЛЮЧ, КОТОРЫЙ НАМ ВЫДАЛ BotFather", request_kwargs=PROXY, use_context=True)
+    mybot = Updater(settings.API_KEY, use_context=True)
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("planet", get_planet_place))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     mybot.start_polling()
