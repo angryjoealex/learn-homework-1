273fb762c046ecb1606467dab1b5e35935e8da72
Aleksei Petrov <aleksei.petrov@five9.com>
2021-09-07 08:11:42 +0300

    в задание 1 добавлна проверка на ввод числа

--- 1_if1.py
+++ 1_if1.py
@@ -29,21 +29,25 @@ def main():
     ]
 
     def check_age(user_age):
-        user_age=float(user_age)
-        if user_age <= 0:
-             print('Вы ввели неверный возраст. Возраст должен быть больше 0. Попробуйте снова')
-             return 0
-        elif user_age >= 130:
-             print('Вы должны быть мертвы')
-             return 1
-        elif  70 <= user_age <130:
-             print('Вы на пенсии')
-             return 1     
-        else :
-            for i in range(len(age_place_list)):
-                if age_place_list[i]['min_age'] <= user_age <= age_place_list[i]['max_age']:
-                    print(f"Ваш возраст {user_age} который соответсвует {age_place_list[i]['place']}")
-                    return 1
+        try:
+            user_age=float(user_age)
+            if user_age <= 0:
+                print('Вы ввели неверный возраст. Возраст должен быть больше 0. Попробуйте снова')
+                return 0
+            elif user_age >= 130:
+                print('Вы должны быть мертвы')
+                return 1
+            elif  70 <= user_age <130:
+                print('Вы на пенсии')
+                return 1
+            else :
+                for i in range(len(age_place_list)):
+                    if age_place_list[i]['min_age'] <= user_age <= age_place_list[i]['max_age']:
+                        print(f"Ваш возраст {user_age} который соответсвует {age_place_list[i]['place']}")
+                        return 1
+        except :
+           print(f"Вы ввели не цифру! Попробуйте снова")
+           return 0
    
     while exit_code != 1:
         user_age=input('Пожалуйста введите свой возраст: ')
